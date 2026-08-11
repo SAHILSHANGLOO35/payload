dd3f768879409ad7f6b02f065c4befb45c7ad54a
SAHILSHANGLOO35 <sahilshangloo35@gmail.com>
2026-08-12 03:02:01 +0530

    Update Receipt icon size class for consistency

--- apps/web/components/icons/receipt.tsx
+++ apps/web/components/icons/receipt.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Receipt = () => {
   return (
     <svg
-      className="size-4.5 text-black dark:text-white"
+      className="size-4.25 text-black dark:text-white"
       width="24"
       height="24"
       viewBox="0 0 24 24"
