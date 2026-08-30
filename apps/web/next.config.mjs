52796975cf8f07820075b42a7f8b92d22f27c07a
SAHILSHANGLOO35 <sahilshangloo35@gmail.com>
2026-08-31 03:10:26 +0530

    next config fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+
+  async rewrites() {
+    const backendUrl = process.env.BACKEND_URL
+
+    if (!backendUrl) {
+      throw new Error("BACKEND_URL is not defined")
+    }
+
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: `${backendUrl}/api/v1/:path*`,
+      },
+    ]
+  },
 }
 
 export default nextConfig
